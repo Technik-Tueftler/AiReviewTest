8f4156f8d552e10a62e22141cb110793fc14222e
Technik Tüftler <technik.tueftler@gmail.com>
2023-09-21 00:05:16 +0200

    add function to check if a date match with an input

--- source/check_summary.py
+++ source/check_summary.py
@@ -1,5 +1,16 @@
 from datetime import datetime, timedelta
 
+def check_matched_day(current_date: datetime, target_day: int) -> bool:
+    last_day_of_current_month = last_day_of_month(current_date)
+    if target_day > last_day_of_current_month.day:
+        if current_date.day == last_day_of_current_month.day:
+            return True
+    else:
+        if current_date.day == target_day:
+            return True
+    return False
+
+
 def last_day_of_month(date) -> datetime:
     if date.month == 12:
         return date.replace(day=31)
@@ -8,6 +19,7 @@ def last_day_of_month(date) -> datetime:
 def main() -> None:
     last_day_of_current_month = last_day_of_month(datetime.utcnow())
     print(last_day_of_current_month)
+
 
 
 if __name__ == "__main__":
